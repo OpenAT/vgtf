0952fe7a80c0f71e63c4f704dbc64bd01cafb96c
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2023-02-07 16:22:10 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/vgtf_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'vgtf_config',
+    'summary': '''FS-Online vgtf instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance vgtf
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
